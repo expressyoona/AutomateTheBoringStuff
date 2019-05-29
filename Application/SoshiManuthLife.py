d207bba88423bff1761611f5d55c196ac0dc471e
Minh Hoang <37230224+expressyoona@users.noreply.github.com>
2019-05-29 10:50:02 +0700

    Bug fixes

--- Application/SoshiManuthLife.py
+++ Application/SoshiManuthLife.py
@@ -6,8 +6,8 @@ def saveFile(url, filename):
 	with open(filename, 'wb') as file:
 		file.write(requests.get(url).content))
 #Get
-URL_link = 'https://soshi.manuth.life/data/soshi/pictures/ohgg/albums/lil-touch/'
-res = requests.get(URL_link)
+URK = 'https://soshi.manuth.life/data/soshi/pictures/ohgg/albums/lil-touch/'
+res = requests.get(URL)
 res.raise_for_status()
 #print(res.text)
 
@@ -15,6 +15,6 @@ soup = bs4.BeautifulSoup(res.text)
 print('----------------')
 linkElems = soup.select('a')
 for i in range(1, len(linkElems)):
-	link_picture = URL_link + linkElems[i].get('href')
+	link_picture = URL + linkElems[i].get('href')
 	saveFile(link_picture, 'Picture_' + str(i) + '.jpg')
 print('Done!')
